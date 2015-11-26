4e8f95b9990285a1c5a019dd2c056cb72975c5c1
18550261681 <369057938@qq.com>
2015-11-26 16:28:08 +0800

    123

---
+++ WpfApplication8/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication8
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDate_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //popChooseDate.IsOpen = true;
+            Pop_xiangji.IsOpen = true;
+        }
+
+        private void txtDate1_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            popChooseDate1.IsOpen = true;
+        }
+
+        private void btn_xiangji_Click(object sender, RoutedEventArgs e)
+        {
+            Pop_xiangji.IsOpen = true;
+        }
+
+        private void btn123_Click(object sender, RoutedEventArgs e)
+        {
+            Pop_xiangji.IsOpen = false;
+        }
+    }
+}
